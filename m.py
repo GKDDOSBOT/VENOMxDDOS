bee0af2d2c08a3355f627f7921f93adb28248f1c
GKDDOSBOT <dilrajsingh123r@gmail.com>
2024-06-24 13:22:20 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7212079650:AAE7aDrPdid6sTlDnq_MmC7qn-aWdHmZ88g')
 
 # Admin user IDs
-admin_id = ["YOUR ID"]
+admin_id = ["1658100619"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
